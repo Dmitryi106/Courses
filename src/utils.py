34c05c1134a4b33f7610392b12dd4e7c3dec1003
Dmitryi106 <156860558+Dmitryi106@users.noreply.github.com>
2024-10-22 17:44:51 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.Product import Product
 
 
 def load_data_from_json(Product):
+    """подгрузка данны из json в класс"""
     categories = []
     with open("Product.json",'r', encoding='UTF-8') as file:
         data = json.load(file)
