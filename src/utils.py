606099fc21eed42c0fda7dc1428efe2ca2a2fd97
ilacker <ilacker106@gmail.com>
2024-10-24 18:10:04 +0700

    Загрузка данных из json

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,8 @@ def load_data_from_json(Product):
             for product_data in category_data['products']:
                 product = Product(product_data['name'], product_data['description'], product_data['price'], product_data['quantity'])
                 products.append(product)
-            category = Category(category_data['name'], category_data['description'], products)
+            category = Category(category_data['name'], category_data['description'], category_data['products'])
             categories.append(category)
         return categories
-print(load_data_from_json(Product))
+loaded = load_data_from_json(Product)
+print(loaded[1].products[0]['name'])
